64f3790f91ac9b370ffd990457def674c5200c03
tenmark86e <tenmark86@gmail.com>
2022-08-16 12:44:38 +0300

    Done Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+//Remove main div
+const main = document.getElementById('main');
+main.remove();
+
+
+//Create new heading element with ID atttribute
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = 'Mark-kilela is the champion';
+
+main.appendChild(newHeader);
